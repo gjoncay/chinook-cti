eac4e8473fc1b97d728bf5fff8a6a6c0f48feb9c
Grant Oncay <gjoncay@gmail.com>
2026-06-27 07:18:25 -1000

    Fix unused Link import causing build failure

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ActorSidebar } from "./ActorSidebar";
 import logoUrl from "../../assets/chinook-logo.png";
 
